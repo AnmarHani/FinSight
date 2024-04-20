2a7d1b54cceab8048871a86bbabf7bf46442b4c6
Recursive-Tahmal <recursivetahmal@gmail.com>
2024-04-20 21:13:07 +0300

    Added transactions to home page and fixed some typos

--- app/src/main/java/com/example/finsight/home_page.java
+++ app/src/main/java/com/example/finsight/home_page.java
@@ -1,6 +1,5 @@
 package com.example.finsight;
 
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -21,12 +20,11 @@ public class home_page extends AppCompatActivity {
 
     private ImageButton newprofileButton;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
-        
+
         // Setup Views Here
 
         new Handler().postDelayed(new Runnable() {
@@ -50,26 +48,23 @@ public class home_page extends AppCompatActivity {
 
             int itemId = item.getItemId();
 
-            if (itemId == R.id.bottom_home){
-
+            if (itemId == R.id.bottom_home) {
 
                 return true;
             }
 
-            else if (itemId == R.id.bottom_transaction){
+            else if (itemId == R.id.bottom_transaction) {
                 startActivity(new Intent(getApplicationContext(), transactions.class));
                 overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
                 finish();
                 return true;
-            }
-            else if (itemId == R.id.bottom_budget){
+            } else if (itemId == R.id.bottom_budget) {
                 startActivity(new Intent(getApplicationContext(), budget.class));
                 overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
                 finish();
 
                 return true;
-            }
-            else if (itemId == R.id.bottom_insight){
+            } else if (itemId == R.id.bottom_insight) {
                 startActivity(new Intent(getApplicationContext(), insights.class));
                 overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
                 finish();
@@ -77,6 +72,8 @@ public class home_page extends AppCompatActivity {
             }
             return false;
         });
+
+        newprofileButton = (ImageButton) findViewById(R.id.profileBtn);
 
         newprofileButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -87,8 +84,6 @@ public class home_page extends AppCompatActivity {
             }
         });
 
-
-
     }
 
     private void fetchAndDisplayUserTransactions() {
@@ -96,7 +91,8 @@ public class home_page extends AppCompatActivity {
             JSONObject response = APIMethods.get(APIMethods.CONNECTION_URL + "/user_transactions");
             if (response.has("result")) {
                 JSONArray transactions = response.getJSONArray("result"); // Get the array
-                for (int i = 0; i < transactions.length(); i++) {
+                int transactionsTextFieldCount = 1;
+                for (int i = transactions.length() - 1; i >= 0; i--) {
                     JSONObject transaction = transactions.getJSONObject(i); // Get each transaction as a JSONObject
                     // Now you can access the fields of each transaction
                     int transactionId = transaction.getInt("transaction_id");
@@ -104,12 +100,24 @@ public class home_page extends AppCompatActivity {
                     String transactionDate = transaction.getString("transaction_date");
                     String description = transaction.getString("description");
                     int transactionType = transaction.getInt("transaction_type");
-
-                    System.out.println(transactionId + " " + amount + " " + transactionDate + " " + description + " " + transactionType);
-
-                    // You can now use these values to update your UI
-                    // For example, add them to a ListView
+                    System.out.println(transactionId + " " + amount + " " + transactionDate + " " + description + " "
+                            + transactionType);
+                    if (transactionsTextFieldCount <= 2) {
+                        int textViewId = getResources().getIdentifier("transaction_name" + transactionsTextFieldCount,
+                                "id", getPackageName());
+                        TextView transaction_name = findViewById(textViewId);
+                        if (description.length() > 18) {
+                            description = description.substring(0, 17) + "...";
+                        }
+                        transaction_name.setText(description);
+                        int transactionAmountId = getResources().getIdentifier(
+                                "transaction_amount" + transactionsTextFieldCount, "id", getPackageName());
+                        TextView transaction_amount = findViewById(transactionAmountId);
+                        transaction_amount.setText(amount + " SR");
+                        transactionsTextFieldCount++;
+                    }
                 }
+
             } else if (response.has("error")) {
                 // Handle error
                 String error = response.getString("error");
@@ -124,10 +132,10 @@ public class home_page extends AppCompatActivity {
         try {
             JSONObject response = APIMethods.get(APIMethods.CONNECTION_URL + "/users_budgets");
             if (response.has("result")) {
-                String budgets = response.getString("result");// I am not sure it is a stirng, it is an array. Please check out how to deal with JSON Arrays? or Print it?
+                String budgets = response.getString("result");// I am not sure it is a stirng, it is an array. Please
+                                                              // check out how to deal with JSON Arrays? or Print it?
                 // Get View and Set Budgets Here as A List View or whatever but limit them to 3
                 // The same this method, use it when clicked on
-
 
             } else if (response.has("error")) {
                 // Handle error
@@ -143,7 +151,8 @@ public class home_page extends AppCompatActivity {
         try {
             JSONObject response = APIMethods.get(APIMethods.CONNECTION_URL + "/current_balance");
             if (response.has("current_balance")) {
-                String currentBalance = response.getString("current_balance"); // Not sure if its String, maybe double or Integer?
+                String currentBalance = response.getString("current_balance"); // Not sure if its String, maybe double
+                                                                               // or Integer?
                 // Get View and Set Current Balance Here
 
             } else if (response.has("error")) {
